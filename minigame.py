dddcb7a34c5bcce1d4e3710403be6b0fe2fd52d0
Daniel Molina <daniel_molina@epam.com>
2026-01-26 15:09:38 -0500

    Basic game x2

--- minigame.py
+++ minigame.py
@@ -2,14 +2,33 @@ import random
 
 
 def rock_paper_scissors():
+
+    rounds = 3
+    round_counter = 0
+    score = {"user": 0, "machine": 0, "ties": 0}
+
+    while round_counter < rounds:
+        user = play()
+        machine = play()
+        print(f"You: {user}")
+        print(f"Machine: {machine}")
+
+        my_results = results(user, machine)
+
+        winner(my_results)
+        print(f"-----------------")
+
+        history(my_results, score)
+        round_counter += 1
+    print(history(results, score))
+
+
+def play():
     rock_paper_scissors = ("rock", "paper", "scissors")
+    return rock_paper_scissors[random.randint(0, 2)]
 
-    user = rock_paper_scissors[random.randint(0, 2)]
-    machine = rock_paper_scissors[random.randint(0, 2)]
 
-    print(f"You: {user}")
-    print(f"Machine: {machine}")
-
+def results(user, machine):
     results = 0
 
     if user == machine:
@@ -26,13 +45,26 @@ def rock_paper_scissors():
         results = -1
     elif user == "scissors" and machine == "paper":
         results = 1
+    return results
 
-    if results == 1:
-        print("You win!")
-    elif results == -1:
-        print("Machine wins!")
+
+def history(result, score):
+    if result == 1:
+        score["user"] += 1
+    elif result == 0:
+        score["ties"] += 1
     else:
-        print("Tied")
+        score["machine"] += 1
+    return score
+
+
+def winner(result):
+    if result == 1:
+        print("You win!")
+    elif result == 0:
+        print("Tie")
+    else:
+        print("Machine wins!")
 
 
 rock_paper_scissors()
