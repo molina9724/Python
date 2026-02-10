75edb38d623fb7532dd561cd5443e12d7537b67e
Daniel Molina <daniel_molina@epam.com>
2026-02-10 09:31:29 -0500

    pangram

--- exercises/book/chapter 6/book.py
+++ exercises/book/chapter 6/book.py
@@ -26,20 +26,52 @@
 # except Exception as e:
 #     print(str(e))
 
-import random
-head_or_tails = ["T", "H"]
-repetitions = 10000
-results = []
+# import random
+# head_or_tails = ["T", "H"]
+# repetitions = 10000
+# results = []
 
-for rep in range(repetitions):
-    coin_flip = list()
-    for cero_to_hundred in range(100):
-        coin_flip.append(random.choice(head_or_tails))
-    index = 0
-    while index in range(len(coin_flip)-5):
-        if all(x == coin_flip[index] for x in coin_flip[index:index+6]):
-            results.append(coin_flip[index:index+6])
-            index += 6
-        index += 1
-print(len(results))
+# for rep in range(repetitions):
+#     coin_flip = list()
+#     for cero_to_hundred in range(100):
+#         coin_flip.append(random.choice(head_or_tails))
+#     index = 0
+#     while index in range(len(coin_flip)-5):
+#         if all(x == coin_flip[index] for x in coin_flip[index:index+6]):
+#             results.append(coin_flip[index:index+6])
+#             index += 6
+#         index += 1
+# print(len(results))
 
+# Pangram Detector
+
+# Write a function named is_pangram(sentence) that accepts a string argument, then returns True if it’s a pangram and False if not. A pangram is a sentence that uses all 26 letters of the alphabet at least once. For example, “The quick brown fox jumps over the yellow lazy dog” is a pangram.
+
+# There are several ways to accomplish this task. One way is to have a variable named EACH_LETTER that starts as an empty list. Then, you can loop over the characters in the string argument, convert each to uppercase with the upper() method, and append it to the EACH_LETTER list if it is a letter and doesn’t already exist there. You can tell that a letter in char isn’t already in the EACH_LETTER list because the expression char not in EACH_LETTER will evaluate to True. After looping over each character in the user’s string, you’ll know that the string is a pangram if len(EACH_LETTER) evaluates to 26.
+
+# For example, the output of your program could look like this:
+
+#     Enter a sentence:
+# The quick brown fox jumps over the yellow lazy dog.
+# That sentence is a pangram.
+
+# Or this:
+
+# Enter a sentence:
+# Hello, world!
+# That sentence is not a pangram.
+
+import string
+
+
+def is_pangram(sentence):
+    clean_sentence = "".join(sentence.lower().split(" "))
+    stripped_sentence = set(clean_sentence)
+    for char in string.ascii_lowercase:
+        if char not in stripped_sentence:
+            return "That sentence is not a pangram."
+    return "That sentence is a pangram."
+
+
+print(is_pangram("Hello there"))
+print(is_pangram("The quick brown fox jumps over the yellow lazy dog."))
