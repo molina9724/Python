9e526348622c2de1f33a9631a201f8b232d08077
Daniel Molina <daniel_molina@epam.com>
2026-03-21 10:01:48 -0500

    auto

--- Playwright Basics/test_practice_round2.py
+++ Playwright Basics/test_practice_round2.py
@@ -8,7 +8,11 @@ def test_05_add_remove_elements(page: Page):
     1. Click the "Add Element" button 3 times
     2. Click one of the "Delete" buttons that appeared
     """
-    pass
+    page.goto("https://the-internet.herokuapp.com/add_remove_elements/")
+    for _ in range(3):
+        page.locator("button[onclick='addElement()']").click()
+    for _ in range(3):
+        page.locator(".added-manually").first.click()
 
 
 def test_06_context_menu(page: Page):
@@ -19,7 +23,11 @@ def test_06_context_menu(page: Page):
     3. A browser alert will pop up. Accept it with: page.on("dialog", lambda d: d.accept())
        (Put the page.on line BEFORE you right-click!)
     """
-    pass
+    page.goto("https://the-internet.herokuapp.com/context_menu")
+    page.on("dialog", lambda d: d.accept())
+    page.locator("#hot-spot").click(button="right")
+
+    # Weird logic
 
 
 def test_07_dynamic_controls(page: Page):
@@ -30,7 +38,11 @@ def test_07_dynamic_controls(page: Page):
     3. Click the "Enable" button under the disabled input
     4. Find the text input and .fill() it with "Playwright is awesome"
     """
-    pass
+    page.goto("https://the-internet.herokuapp.com/dynamic_controls")
+    page.locator("input[type='checkbox']").check()
+    page.locator("button[onclick='swapCheckbox()']").click()
+    page.locator("button[onclick='swapInput()']").click()
+    page.locator("input[type='text']").fill("Playwright is awesome")
 
 
 def test_08_basic_auth(page: Page):
@@ -52,7 +64,13 @@ def test_09_multiple_windows(page: Page):
        new_page = popup_info.value
     3. Now use new_page (not page!) to read the new tab's content
     """
-    pass
+    page.goto("https://the-internet.herokuapp.com/windows")
+    with page.expect_popup() as popup_info:
+        page.locator("a[href='/windows/new']").click()
+    new_page = popup_info.value
+    new_page.goto("https://the-internet.herokuapp.com/windows")
+    page.bring_to_front()
+    page.goto("https://playwright.dev/python/docs/api/class-locator#locator-press")
 
 
 def test_10_javascript_alerts(page: Page):
@@ -65,4 +83,11 @@ def test_10_javascript_alerts(page: Page):
        page.on("dialog", lambda d: d.accept("Hello from Playwright!"))
        (Set this handler, then click the "Click for JS Prompt" button)
     """
-    pass
+    page.goto("https://the-internet.herokuapp.com/javascript_alerts")
+    page.once("dialog", lambda d: d.accept())
+    page.locator("button[onclick='jsAlert()']").click()
+    page.once("dialog", lambda d: d.accept())
+    page.locator("button[onclick='jsConfirm()']").click()
+
+    page.once("dialog", lambda d: d.accept("Hello from Playwright!"))
+    page.locator("button[onclick='jsPrompt()']").click()
