b6fb80eecd48342b95697fc38f3a328128e0864e
Daniel Molina <molina9724@gmail.com>
2026-05-02 10:39:37 -0500

    Enhance exercises.py by adding detailed cat information retrieval and pretty-printing for black cats; add new SQL session file for querying cats with specific attributes.

--- Book/chapter16/exercises.py
+++ Book/chapter16/exercises.py
@@ -4,4 +4,35 @@ conn = sqlite3.connect(
     "/Users/daniel_molina/Downloads/Python/Python/Book/chapter16/example.db",
     isolation_level=None,
 )
-print(conn.execute('SELECT name FROM sqlite_schema WHERE type="table"').fetchall())
+# print(conn.execute('SELECT name FROM sqlite_schema WHERE type="table"').fetchall())
+
+# conn.execute("INSERT INTO cats VALUES ('Perseo', '1997-01-24', 'orange', 10)")
+
+# # Fuck hackers
+# cat_name = "Ranger"
+# cat_birthday = "1990-12-12"
+# fur_color = "black"
+# cat_weight = 12
+# conn.execute(
+#     "INSERT INTO cats VALUES (?,?,?,?)",
+#     [
+#         cat_name,
+#         cat_birthday,
+#         fur_color,
+#         cat_weight,
+#     ],
+# )
+
+print(conn.execute("SELECT * FROM cats").fetchall())
+print(conn.execute("SELECT rowid,name FROM cats").fetchall())
+print(conn.execute("SELECT * FROM cats ORDER BY name asc").fetchall())
+
+all_cats = conn.execute("SELECT * FROM cats ORDER BY name asc")
+for cat in all_cats:
+    print(f"Name: {cat[0]}, Birthdate: {cat[1]}, Fur: {cat[2]}, Weight: {cat[3]}")
+
+import pprint
+
+pprint.pprint(
+    conn.execute("SELECT * FROM cats WHERE fur = 'black' ORDER BY name ASC").fetchall()
+)
---
+++ chapter16-example.session.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM cats
+WHERE fur = 'black'
+    AND weight_kg >= 6
+ORDER BY name ASC;
