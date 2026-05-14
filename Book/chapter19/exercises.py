a123419fda0d83b43c6c55d3c8a2cd1b1efb0237
Daniel Molina <molina9724@gmail.com>
2026-05-14 16:03:42 -0500

    Add additional datetime examples and calculations in exercises.py

--- Book/chapter19/exercises.py
+++ Book/chapter19/exercises.py
@@ -44,3 +44,20 @@ print(dt.day)
 print(dt.hour)
 print(dt.minute)
 print(dt.second)
+
+print(datetime.datetime.fromtimestamp(time.time()))
+print(datetime.datetime.fromtimestamp(1000000000))
+
+halloween_2026 = datetime.date(2026, 10, 31)
+new_years_2027 = datetime.date(2027, 1, 1)
+
+print(halloween_2026 > new_years_2027)
+print(new_years_2027 - halloween_2026)
+
+delta = datetime.timedelta(days=11, hours=10, minutes=9, seconds=8)
+print(delta.days, delta.seconds, delta.microseconds)
+print(delta.total_seconds())
+print(delta)
+
+thousand_days = datetime.timedelta(days=1000)
+print(halloween_2026 + thousand_days)
